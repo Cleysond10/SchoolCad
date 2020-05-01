a91d9a919d92ca1eb6f077193fed2267228d3ca9
Gerson Neto <jgsolucoestecnologicas@gmail.com>
2020-05-01 20:03:01 -0300

    projeto Inicial

---
+++ src/Aplicacao/MainSchool.java
@@ -0,0 +1,47 @@
+package Aplicacao;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Neto
+ */
+public class MainSchool extends Application {
+    
+    private static Stage stage;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            stage = primaryStage;
+
+            AnchorPane fxmlMenu = (AnchorPane)FXMLLoader.load(getClass().getResource("MainSchool.fxml"));
+            Scene scene = new Scene(fxmlMenu,800,600);
+            
+            stage.setTitle("SCHOOLCAD");
+            stage.setScene(scene);
+            stage.show();
+            setStage(stage);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }    
+    }
+
+    public static Stage getStage() {
+        return stage;
+    }
+
+    public static void setStage(Stage stage) {
+        MainSchool.stage = stage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
+
